9f3a129aa84cfe27063abd232776770a8a305f74
Roy Chen (陳彥廷) <roycyt@gmail.com>
2022-04-17 14:09:02 +0000

    Disable plugin: vim-rhubarb

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | Packer
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Package manager
   use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+  use { 'tpope/vim-rhubarb', cond = false } -- Fugitive-companion to interact with github
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use { 'ludovicchabant/vim-gutentags', cond = false } -- Automatic tags management
   -- UI to select things (files, grep results, open buffers...)
